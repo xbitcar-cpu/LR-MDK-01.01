86ca511168994f68ac0fe323ab81de7ebd2fc8e3
VadimKuvaldaev <VadimKuvaldaev@gmail.com>
2026-09-08 10:47:53 +0300

    zadacha 1.3

--- 5 zadacha/5 zadacha/Program.cs
+++ 5 zadacha/5 zadacha/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Schema;
 
 namespace _5_zadacha
 {
@@ -11,10 +12,21 @@ namespace _5_zadacha
         static void Main(string[] args)
         {
             Console.WriteLine("Введите 3 цифры");
-           int a =int.Parse(Console.ReadLine());
-           int b =int.Parse(Console.ReadLine());
-           int c =int.Parse(Console.ReadLine());          
-                                              
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+                int max= a;
+            if(b> max)
+            {
+                max = b;
+            }
+            if (c > max) 
+            {
+                c = max;
+            }
+            Console.WriteLine("Максимальное число");
+            Console.WriteLine(max);
+            
        }
     }
 }
